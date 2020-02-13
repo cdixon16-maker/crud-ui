dfc686b27ddba25026fef29960d854f7c79cab1c
Chase Dixon <schasedixon16@gmail.com>
2020-02-12 19:35:18 -0700

    List Component

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -3,8 +3,10 @@ import React from "react";
 export default function BookList(props) {
     return (
         <div>
-            <h1>{props.title}</h1>
-    <h1>{props.Author}</h1>
+            <ul>
+                <li>{props.title}</li>
+                <li>{props.Author}</li>
+            </ul>
         </div>
     )
 }
